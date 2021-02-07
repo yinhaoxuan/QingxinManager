fc1c3e1a1f3179ae6b826d8899b725ed182439f9
Haoxuan Yin <1723010279@qq.com>
2021-02-07 13:12:04 +0800

    add models

--- article/models.py
+++ article/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 
+class Person(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=10)
+    member = models.ManyToManyField(Person)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=50000)
+    author = models.ManyToManyField(Person, related_name='wrote')
+    editor = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='edited')
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
