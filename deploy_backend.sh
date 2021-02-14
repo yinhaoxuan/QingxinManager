b328178dd595f5b2b46a4ddbaa16334205ef1a95
Haoxuan Yin <1723010279@qq.com>
2021-02-14 18:00:15 +0800

    correct deploy sh

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,1 +1,2 @@
-gunicorn QingxinManager.wsgi -b 0:8000
+sudo fuser -k 8000/tcp
+gunicorn QingxinManager.wsgi -b 0:8000 &
--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -1,4 +1,5 @@
+sudo fuser -k 5000/tcp
 cd frontend
 npm run build
 npm install -g serve
-serve -s build
+serve -s build &
